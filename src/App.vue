a0f577a12692c8bccbd0a06975bd75b817ec3cb2
timpuyda <31927275+timpuyda@users.noreply.github.com>
2021-04-21 11:00:32 +0300

    finished programming

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,205 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+      <div class="logo">
+        <img src="@/assets/images/LOGO.svg" alt="CRYPTO">
+      </div>
+    </div>
+   
+    <div class="main-content">
+      <!-- <div > -->
+        <h1 class="container dark-grey d-md-none">Панель котировок</h1>
+      <!-- </div> -->
+      <div class="plot">
+        <div class="currency-title">
+          <p class="h1">USD | {{currentCurrency}}</p>
+          <div id="chart">
+            <amchart
+            v-if="ohlcvData"
+            :chartProps="ohlcvData"
+            :socketRecieve="dataRecieved">
+            </amchart>
+             <!-- <button @click="add">add</button> -->
+             <br>
+             <br>
+             <button @click="closeSocket">Close socket</button>
+          </div>
+        </div>
+      </div>
+      <div class="left-side">
+        <h1 class="container dark-grey d-none d-md-block">Панель котировок</h1>
+        <table class="table">
+          <thead>
+           <tr>
+             <th>Валютные пары</th>
+             <th>Изменение</th>
+             <th>Цена</th>
+           </tr>
+          </thead>
+          <tbody>
+            <tr 
+            v-for="currency in currencies"
+            :key="currency.name"
+            @click="changeCurrency(currency.name)"
+            :class="{active:currency.name==currentCurrency}"
+            >
+              <td>USD | {{currency.name}}</td>
+              <td>
+                <span class="icon"><img :src="require('./assets/images/'+currency.icon+'.svg')" alt=""></span>
+              {{currency.change | toPrecision(2)}} %</td>
+              <td> {{currency.price}} </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import amchart from './components/chart';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    amchart
+  },
+  data() {
+    return {
+      currentCurrency: 'BTC',
+      currencies: [
+        {
+          name: 'BTC',
+          change: '0',
+          icon: 'up',
+          price: '57093.95',
+        },
+        {
+          name: 'BCH',
+          change: '0',
+          icon: 'up',
+          price: '964',
+        },
+        {
+          name: 'ETH',
+          change: '0',
+          icon: 'up',
+          price: '964',
+        },
+        {
+          name: 'XRP',
+          change: '0',
+          icon: 'up',
+          price: '964',
+        }
+      ],
+      apiKey: process.env.VUE_APP_KEY,
+      baseUrl: 'wss://streamer.cryptocompare.com/v2',
+      restUrl:'https://min-api.cryptocompare.com/data/v2/histominute',
+      dataRecieved: null,
+      ohlcvData: null,
+      subscriptionMessage: {
+          "action": "SubAdd",
+          "subs": ["0~Coinbase~BTC~USD", "0~Coinbase~BCH~USD", "0~Coinbase~ETH~USD","0~Coinbase~XRP~USD"]
+      },
+      unSubscriptionMessage: {
+          "action": "SubRemove",
+         "subs": ["0~Coinbase~BTC~USD", "0~Coinbase~BCH~USD", "0~Coinbase~ETH~USD","0~Coinbase~XRP~USD"]
+      },
+      socket: null
+
+    }
+  },
+  mounted() {
+    this.getOHLCV(this.currentCurrency);
+    this.recieveCurrentData()
+  },
+  methods: {
+    changeCurrency(val){
+       this.currentCurrency=val;
+      this.getOHLCV(val);
+      // this.unsubscribe(val.unsubscriptionMessage);
+      // this.recieveCurrentData(val.subscriptionMessage)
+    },
+    recieveCurrentData(){
+      const socket = new WebSocket(this.baseUrl+'?api_key='+this.apiKey);
+      socket.onopen =() => {
+        socket.send(JSON.stringify(this.subscriptionMessage))
+      }
+      socket.onmessage = (e) => {
+        const msg = JSON.parse(e.data)
+        // console.log(msg);
+        this.currencies = this.currencies.map((item)=>{
+          if (item.name===msg.FSYM){
+            
+            let diff = msg.P-item.price
+            if((diff) !== 0){
+              diff>0?item.icon='up':item.icon='down';
+
+              item.change = Math.round((diff)/item.price *100 *100)/100;
+              item.price = msg.P;
+            }
+            // console.log('msg.P:',msg.P,'item.price: ',item.price, ' (msg.P-item.price)/item.price',(msg.P-item.price)/item.price);
+         }
+          return item 
+        })
+        if (msg.TYPE==='0' && msg.FSYM===this.currentCurrency){
+          this.dataRecieved=msg;
+           console.log('fine: ',msg.FSYM);
+          //  console.log(msg);
+        }else{
+          console.log(`debug ${msg.TYPE}`);
+          // console.log(msg)
+        }
+      }
+      socket.onclose = (e) => {
+        console.log(`${e.code}; ${e.reason}; from mount`);
+      }
+      socket.onerror = (err) => console.log(err.message);
+      this.socket = socket;
+    },
+    closeSocket(){
+      if (this.socket){
+
+        console.log('close click');
+        this.socket.send(JSON.stringify(this.unSubscriptionMessage))
+        this.socket.close(1000, 'btn clicked')
+        this.socket.onclose = (e) => {
+          console.log(`${e.code}; ${e.reason} reactive`);
+        }
+      }
+    },
+    // unsubscribe(){
+    //   this.socket.send(JSON.stringify(this.unSubscriptionMessage))
+    //   console.log('this.unsubscribe:',JSON.stringify(unsubscribeMsg));
+    // },
+    getOHLCV(cur){
+      const options = {
+        params: {
+          fsym: cur,
+          tsym: 'USD',
+          limit: 99,
+        }
+      }
+      this.$axios.get(this.restUrl,options)
+      .then((res)=>{
+          console.log(res);
+          // console.log( Array.isArray(res.data.Data.Data))
+          this.ohlcvData = res.data.Data.Data;
+        })
+      .catch((err)=>console.dir(err))
+    }
+  },
+  filters: {
+    toPrecision(val,to){
+      return Number(val).toFixed(to);
+    }
   }
 }
+
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import './assets/scss/main.scss';
 </style>
---
+++ src/components/chart.vue
@@ -0,0 +1,104 @@
+<template>
+ <div class="chartdiv" ref="chartdiv">
+ </div>
+</template>
+
+<script>
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+am4core.useTheme(am4themes_animated);
+
+export default {
+  name: 'Chart',
+  props: {
+    chartProps:{
+      type: Array,
+      default: ()=>[]
+    },
+    socketRecieve: {
+      type: Object,
+      // type: Array,
+      // default: ()=>[]
+    }
+  },
+  data(){
+    return {
+      chartData: [],
+      chart: null,
+      chartCurrentData:{}
+    }
+  },
+  
+ mounted() {
+    this.init();
+  },
+  methods: {
+    destroy(){
+       this.chart.dispose();
+    },
+    init(){
+      let chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
+
+        chart.paddingRight = 30;
+
+      this.chartData = this.chartProps.map(item =>{ 
+        return {
+          time: item.time*1000,
+          val: item.close
+        }
+        });
+        chart.data = this.chartData;
+
+        let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+        dateAxis.renderer.grid.template.location = 0;
+
+        let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.tooltip.disabled = true;
+        valueAxis.renderer.minWidth = 35;
+
+        let series = chart.series.push(new am4charts.LineSeries());
+        series.dataFields.dateX = "time";
+        series.dataFields.valueY = "val";
+
+        series.tooltipText = "{valueY.value}";
+        chart.cursor = new am4charts.XYCursor();
+
+        let scrollbarX = new am4charts.XYChartScrollbar();
+        scrollbarX.series.push(series);
+        chart.scrollbarX = scrollbarX;
+
+        this.chart = chart;
+      }
+  },
+  watch:{
+    socketRecieve() {
+      if (this.chart){
+        // this.chartCurrentData=.map(item=>{
+        //   return {
+        //     val:item.P,
+        //     time: item.TS
+        //   }
+        // })
+        this.chartCurrentData.val = this.socketRecieve.P;
+        this.chartCurrentData.time = this.socketRecieve.TS*1000;
+        this.chart.addData(this.chartCurrentData)
+      }
+    },
+    chartProps(){
+      this.init()
+    }
+      
+  },
+  beforeDestroy() {
+    if (this.chart) {
+     this.destroy()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
